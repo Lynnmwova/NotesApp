1ff1214e7847473dec492e83e07378992fa22aef
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2022-09-26 11:35:29 +0300

    updateJS

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+/* Declaration of variables */
 const addBox = document.querySelector(`.add-box`);
 popupBox = document.querySelector(`.popup-box`);
 popupTitle = document.querySelector(`header p`);
 closeIcon = document.querySelector(`header i`);
 titleEl = document.querySelector(`input`);
+descEl = document.querySelector(`textarea`);
+addBtn = document.querySelector(`button`);
+
+const months =['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const notes = JSON.parse(localStorage.getItem(`notes`) || '[]');
+let isUpdate = false, updateId;
+
+/* Start of first function */
+function showNotes(){
+    
+}
