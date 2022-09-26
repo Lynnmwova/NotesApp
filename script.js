8ecb3d840498e87743e0aea6ae89f787ff93da7f
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2022-09-26 11:24:28 +0300

    StartJS

---
+++ script.js
@@ -0,0 +1,5 @@
+const addBox = document.querySelector(`.add-box`);
+popupBox = document.querySelector(`.popup-box`);
+popupTitle = document.querySelector(`header p`);
+closeIcon = document.querySelector(`header i`);
+titleEl = document.querySelector(`input`);
